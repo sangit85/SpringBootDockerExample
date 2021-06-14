f029187dbca291efd8df61ad55682e1c7f0be5e2
sangit85 <santoshk.github@gmail.com>
2021-06-14 11:26:02 +0530

    commit

---
+++ src/main/java/com/example/demo/SpringBootDockerExampleApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerExampleApplication.class, args);
+		
+			System.out.println("Hello world");
+		
+		
+	}
+
+}
